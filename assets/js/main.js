64260a349ef8ca67270e291f0c944d9296d35173
kerimovok <naxro.ok1@gmail.com>
2024-03-23 17:28:01 +0400

    Added: Events section

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,64 @@ $(document).ready(function () {
         nextArrow:
             '<button class="arrow-slider next-slider"><i class="fas fa-angle-right" aria-hidden="true"></i></button>',
     });
+
+    $(".events-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        dots: true,
+        dotsClass: "slider-dots",
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        arrows: true,
+        prevArrow:
+            '<button class="arrow-slider prev-slider"><i class="fas fa-angle-left" aria-hidden="true"></i></button>',
+        nextArrow:
+            '<button class="arrow-slider next-slider"><i class="fas fa-angle-right" aria-hidden="true"></i></button>',
+    });
+
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+            total: t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+        };
+    }
+
+    function initializeClock(id, endtime) {
+        var daysSpan = $(".days");
+        var hoursSpan = $(".hours");
+        var minutesSpan = $(".minutes");
+        var secondsSpan = $(".seconds");
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+            daysSpan.html(t.days);
+            hoursSpan.html(("0" + t.hours).slice(-2));
+            minutesSpan.html(("0" + t.minutes).slice(-2));
+            secondsSpan.html(("0" + t.seconds).slice(-2));
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+
+    var deadline = new Date(
+        Date.parse(new Date()) + 25 * 24 * 60 * 60 * 1000 + 13 * 60 * 60 * 1000,
+    );
+
+    initializeClock("clockdiv", deadline);
 });
