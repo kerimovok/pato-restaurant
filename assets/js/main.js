880b5d82fdb8ff055f56daf2fbe521c40a010fa7
kerimovok <naxro.ok1@gmail.com>
2024-03-24 02:48:54 +0400

    Added: Reviews section

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,23 @@ $(document).ready(function () {
             '<button class="arrow-slider next-slider"><i class="fas fa-angle-right" aria-hidden="true"></i></button>',
     });
 
+    $(".reviews-slider").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // fade: true,
+        dots: true,
+        dotsClass: "slider-dots",
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        arrows: true,
+        prevArrow:
+            '<button class="arrow-slider prev-slider"><i class="fas fa-angle-left" aria-hidden="true"></i></button>',
+        nextArrow:
+            '<button class="arrow-slider next-slider"><i class="fas fa-angle-right" aria-hidden="true"></i></button>',
+    });
+
+    // COUNTDOWN
     function getTimeRemaining(endtime) {
         var t = Date.parse(endtime) - Date.parse(new Date());
         var seconds = Math.floor((t / 1000) % 60);
